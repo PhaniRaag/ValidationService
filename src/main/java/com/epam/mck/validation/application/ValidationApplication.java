205210ffd6bcc2a308f5151dfa46321b35e6c5d1
PhaniRaag <happy.achanta@gmail.com>
2017-11-30 14:45:39 +0530

    Update ValidationApplication.java

--- src/main/java/com/epam/mck/validation/application/ValidationApplication.java
+++ src/main/java/com/epam/mck/validation/application/ValidationApplication.java
@@ -14,7 +14,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-public class ValidationApplication {
+public class ValidationApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ValidationApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ValidationApplication.class, args);
 	}
