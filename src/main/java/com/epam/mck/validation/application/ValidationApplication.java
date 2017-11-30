8230bc79482be096c9e30a51e49b8f5b319bccad
PhaniRaag <happy.achanta@gmail.com>
2017-11-30 14:56:06 +0530

    Update ValidationApplication.java

--- src/main/java/com/epam/mck/validation/application/ValidationApplication.java
+++ src/main/java/com/epam/mck/validation/application/ValidationApplication.java
@@ -4,7 +4,7 @@ package com.epam.mck.validation.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
